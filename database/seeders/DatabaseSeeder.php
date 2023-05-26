d93a05a50e0b2c51d1a7e748c0a54c11d671adcb
Simon Roland <simon@uptime.dk>
2023-05-26 13:30:13 +0200

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->count(9)->create();
+
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('12345'),
+            'created_at' => now()->subDays(10),
+        ]);
+    }
+}
